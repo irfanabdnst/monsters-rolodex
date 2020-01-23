c1190e52956bb6ba2eeca03bfa89c77ba6bef4a0
irfanabdnst <irfan.abdillah@gmail.com>
2020-01-23 21:00:55 +0700

    map() + key attribute

--- src/App.js
+++ src/App.js
@@ -2,28 +2,34 @@ import './App.css';
 
 import React, { Component } from 'react';
 
-import logo from './logo.svg';
-
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      string: 'Hello Vesna'
+      monsters: [
+        {
+          id: 'asc1',
+          name: 'Frankenstein'
+        },
+        {
+          id: 'asc2',
+          name: 'Dracula'
+        },
+        {
+          id: 'asc3',
+          name: 'Zombie'
+        }
+      ]
     };
   }
 
   render() {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: 'Hello Irfan' })}>Change</button>
-          <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-            Learn React
-          </a>
-        </header>
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}> {monster.name} </h1>
+        ))}
       </div>
     );
   }
