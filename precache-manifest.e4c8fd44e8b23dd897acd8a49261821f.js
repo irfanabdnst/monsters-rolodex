14b6a182e5a6c7d62b2158da5d5f521d3f3bd2b3
irfanabdnst <irfan.abdillah@gmail.com>
2020-01-25 00:54:21 +0700

    Updates

---
+++ precache-manifest.e4c8fd44e8b23dd897acd8a49261821f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "936bff93b840354e7d2776c93cc41fa3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e1070830ee26d625c8eb",
+    "url": "/monsters-rolodex/static/css/main.e2493496.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1070830ee26d625c8eb",
+    "url": "/monsters-rolodex/static/js/main.0b014dfa.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
